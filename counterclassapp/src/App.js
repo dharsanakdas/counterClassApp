d2c321541622321945f736b5b3af2c6084b2357b
dharsana k das <dharsanakdas@gmail.com>
2026-01-11 12:48:59 +0530

    new changes

---
+++ counterclassapp/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1
+    }));
+  };
+
+  render() {
+    return (
+      <div className="appContainer">
+                <h1>Counter App</h1>
+
+        <p>Count: {this.state.count}</p>
+
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default App;
